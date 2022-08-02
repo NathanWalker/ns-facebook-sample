14b642785f60f1e08f887bf3f62e495ca2ed2464
Meg Seegmiller <mseegmiller@hhomesltd.com>
2022-08-02 12:54:55 -0500

    myCoolApp with Firebase and Facebook

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 import Vue from 'nativescript-vue'
 
 import Home from './components/Home'
+import { LoginManager } from '@nativescript/facebook';
+LoginManager.init();
 
 new Vue({
   render: (h) => h('frame', [h(Home)]),
--- app/components/Home.vue
+++ app/components/Home.vue
@@ -11,16 +11,60 @@
                     <Span :text="message"/>
                 </FormattedString>
             </Label>
+            <button text="Facebook" @tap="facebookLogin" />
         </GridLayout>
     </Page>
 </template>
 
 <script>
+import { LoginManager } from '@nativescript/facebook';
+import {
+    firebase
+} from '@nativescript/firebase-core';
+import '@nativescript/firebase-analytics';
   export default {
     computed: {
       message() {
         return "Blank {N}-Vue app";
       }
+    },
+    methods: {
+
+        async facebookLogin() {
+            let vm = this;
+            await LoginManager.logInWithPermissions(['public_profile']).then(profile => {
+                console.log('profile', profile);
+                if (profile.Id) {
+                    vm.external = 'facebook';
+                    vm.external_id = result.Id;
+                    vm.first_name = result.ProfileDataFields.first_name;
+                    vm.last_name = result.ProfileDataFields.last_name;
+                    vm.email = result.ProfileDataFields.email;
+                    vm.login(vm.external, {
+                        external: vm.external,
+                        id: vm.external_id
+                    });
+                } else {
+                    try {
+                        firebase().analytics().logEvent('log_error', {
+                            'template': 'Login',
+                            'code': 1,
+                            'message': "Error while logging in to Facebook: " + e
+                        });
+                    } catch (e) {}
+                }
+            }).catch(e => {
+                console.log('Facebook login error:', e);
+                try {
+                    firebase().analytics().logEvent('log_error', {
+                        'template': 'Login',
+                        'code': 2,
+                        'message': "Error while logging in to Facebook: " + e
+                    });
+                } catch (e) {}
+            });
+
+        },
     }
   };
 </script>
